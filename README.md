f6b869e305f2da44e0b7564a7c309c54b6586b78
luckman212 <1992842+luckman212@users.noreply.github.com>
2024-12-24 12:23:28 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# unifi-stp-prio
+<img src=stp_icon.png width=200>
+
+## stp_prio
+
+Python wrapper around the Unifi API to retrieve (R)STP priority and other info of the USW switches for a site.
+
+### Setup and Usage
+
+Requires a semi-recent Python with the `requests` module installed.
+
+Edit the script to adjust the variables at the top such as site id, Unifi controller hostname, and API login credentials.
+
+Run with `-h` or `--help` for help on the commandline arguments.
+
+```
+$ ./stp_prio.py -h
+usage: stp_prio.py [--site SITE]
+
+Show Unifi switch STP priority
+
+options:
+  --site, -s SITE  Site ID (as shown in Unifi web interface)
+```
+
+### List of Environment Variables
+
+- `UNIFI_HOST` the hostname and port of your Unifi controller e.g. `unifi.foo.com:8443`
+- `UNIFI_USER` a user to authenticate with for API calls (typically `admin`)
+- `UNIFI_PASS` password (_yes_ I know it's not a good idea to store passwords in the clear! Please fork the script and improve the security by using a password manager, cryptography or whatever other method you see fit!)
+
+### Other Variables (hardcoded)
+
+Can be overridden using argument `--site/-s`.
+
+- `DEFAULT_SITE` = The default site to operate on (you will see this if you navigate to your Unifi controller, it is the part after the `/manage/` in the URL)
+
+### Example Helper Script
+
+Here's a small helper script example for storing credentials and vars in a separate file:
+
+```
+#!/bin/sh
+
+export UNIFI_HOST=unifi.mycorp.net:8443
+export UNIFI_USER=admin
+export UNIFI_PASS=supersecret123
+
+/usr/local/bin/stp_prio.py \
+  --site ioj7qtxy \
+  "$@"
+```
+
+### Can this program do _xyz_ ?
+
+I wrote this to scratch my own itch. I've tested it fairly extensively on my own setup, but YMMV. I am running UNA version 9.0.106 as of this writing. Bugreports or PRs to add features welcome!
---
+++ stp_prio.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import requests
+import argparse
+
+DEFAULT_HOST = 'unifi-controller-url:8443'
+DEFAULT_USER = 'admin'
+DEFAULT_PASS = 'hunter2'
+DEFAULT_SITE = 'abcdwxyz'
+
+class Unifi:
+    def __init__(self, host, username, password):
+        self.host = host
+        self.username = username
+        self.password = password
+        self.session = requests.Session()
+        self.csrf = ''
+
+    def login(self):
+        payload = {'username': self.username, 'password': self.password}
+        r = self.request('/api/login', payload)
+        if not r.ok:
+            print(f'Login failed at {self.host}/api/login: {r.text}', file=sys.stderr)
+        return r.ok
+
+    def request(self, path, data=None, method='POST'):
+        if data is None:
+            data = {}
+        method = method.lower()
+        if method not in ['get', 'post', 'put', 'delete']:
+            raise ValueError(f'Unsupported HTTP method: {method}')
+        uri = f'https://{self.host}{path}'
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        if self.csrf:
+            headers['X-CSRF-Token'] = self.csrf
+        r = getattr(self.session, method)(
+            uri, json=data, verify=True, headers=headers
+        )
+        self.csrf = r.headers.get('X-CSRF-Token', self.csrf)
+        return r
+
+def validate_response(res) -> bool:
+    try:
+        response_data = res.json()
+        if res.status_code == 200 and response_data.get('meta', {}).get('rc') == 'ok':
+            return True
+    except ValueError:
+        pass
+    print(f'Error while accessing {res.url}: {res.text}', file=sys.stderr)
+    return False
+
+def get_values(device, keys, default='-'):
+    return [device.get(key, default) for key in keys]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=False,
+        description='Show Unifi switch STP priority')
+    parser.add_argument('--help','-h',
+        action='help',
+        help=argparse.SUPPRESS)
+    parser.add_argument('--site','-s',
+        default=DEFAULT_SITE,
+        help='Site ID (as shown in Unifi web interface)')
+    args = parser.parse_args()
+
+    SITE = args.site.lower() if args.site else DEFAULT_SITE
+    HOST = os.getenv('UNIFI_HOST', DEFAULT_HOST)
+    USER = os.getenv('UNIFI_USER', DEFAULT_USER)
+    PASS = os.getenv('UNIFI_PASS', DEFAULT_PASS)
+
+    sess = Unifi(host=HOST, username=USER, password=PASS)
+    try:
+        if not sess.login():
+            exit(2)
+    except Exception as e:
+        print(e, file=sys.stderr)
+        exit(1)
+
+    res = sess.request(f'/api/s/{SITE}/stat/device', method='get')
+    if not validate_response(res):
+        exit(1)
+    switches = [dev for dev in res.json().get('data') if dev.get('type') == 'usw']
+    keys = ['name', 'stp_priority', 'stp_version', 'shortname', 'mac', 'ip', 'version']
+    headers = ['device name', 'prio', 'type', 'model', 'MAC', 'IP', 'fw version']
+    rows = [headers]
+    for s in sorted(switches, key=lambda x: int(x['stp_priority']), reverse=False):
+        rows.append(get_values(s, keys))
+    col_widths = [max(len(str(row[i])) for row in rows) for i in range(len(headers))]
+    col_widths = [width + 2 for width in col_widths]
+    header_line = ''.join(header.ljust(col_widths[i]) for i, header in enumerate(headers))
+    data_lines = [
+        ''.join(str(row[i]).ljust(col_widths[i]) for i in range(len(row)))
+        for row in rows[1:]
+    ]
+
+    print(header_line)
+    print('─' * len(header_line))
+    print('\n'.join(data_lines))
